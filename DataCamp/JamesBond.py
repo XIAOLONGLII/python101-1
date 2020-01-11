18600f3b21eb0e87f2e183f61f8f55bc1cfe8f66
XIAOLONG LI <1491skdpf89@gmail.com>
2020-01-11 18:38:58 -0500

    Update JamesBond.py

--- DataCamp/JamesBond.py
+++ DataCamp/JamesBond.py
@@ -1,5 +1,7 @@
-# Author: Maria Malave
-#Date: 01/10/2020
+"""
+Author: Maria Malave
+Date: 01/10/2020
+"""
 
 def greet(first_name, last_name):
     greeting = 'My name is ' + last_name + ', ' + first_name + ' ' + last_name + '!'
@@ -7,4 +9,9 @@ def greet(first_name, last_name):
 
 # Replace with your first and last name.
 # That is, unless your name is already James Bond.
-greet('James', 'Bond')
+print(greet('James', 'Bond'))
+
+"""
+result:
+My name is Bond, James Bond!
+"""
